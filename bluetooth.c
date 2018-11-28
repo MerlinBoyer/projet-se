ac0663878c9fc7f4ec8d3c6e9a23bf32f3800bb5
MerlinBoyer <merlin.boyer@wanadoo.fr>
2018-11-28 09:11:43 +0100

    ble init ok

--- bluetooth.c
+++ bluetooth.c
@@ -1,79 +1,81 @@
-#define BAUD 115200
+#define FOSC 13000000 // clk freq
+#define BAUD 38400
 #include <avr/io.h>
 #include <util/delay.h>
 #include <util/setbaud.h>
 #define MAXBUF 999
-#define MYUBRR F_CPU/16/BAUD-1
-
+#define MYUBRR FOSC / 8 / BAUD - 1
 
 /////   Setup Usart for Ble device com  //////
 
-void USART_init(){
-  /*Set baud rate */
-  UBRR0H = UBRRH_VALUE; // high value
-  UBRR0L = UBRRL_VALUE;  // low value
-  UCSR0A = 0;
-  UCSR0B = (1<<RXEN0) | (1<<TXEN0); // as writer and reader
-  UCSR0C = (3<<UCSZ00); // 8 bit for caracter size
+void USART_init(unsigned int ubrr)
+{
+  /* Set baud rate */
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+  /* Enable receiver and transmitter */
+  UCSR0A = (1 << U2X);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  /* Set frame format: 8data, 2stop bit */
+  UCSR0C = (3 << UCSZ00);
 }
 
-// void USART_init(){
-//     UBRR0H = (unsigned char)0x00;
-//     UBRR0L = (unsigned char)0x10; //baudrate at 115200
-
-//     UCSR0A = (_BV(U2X0));
-//     UCSR0B = (_BV(RXEN0)) | (_BV(TXEN0)) | (_BV(RXCIE0)); //enable RX and Tx
-//     UCSR0C = (3 << (UCSZ00));                             //8bit char
-// }
-
-
-static void USART_send_char( unsigned char c ){
-  while ( !( UCSR0A & (1<<UDRE0)) );
-  UDR0 = 'r';
-  while ((UCSR0A & (1 << TXC0)) == 0x00);
+static void USART_send_char(unsigned char c)
+{
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
+  UDR0 = c;
 }
 
-static unsigned char USART_get_char( void ){
+static unsigned char USART_get_char(void)
+{
   /* Wait for data to be received */
-  while (!(UCSR0A & (1<<RXC0)));
+  while (!(UCSR0A & (1 << RXC0)))
+    ;
   /* Get and return received data from buffer */
   return UDR0;
 }
 
-static void USART_get_data(char * out){
+static void USART_get_data(char *out)
+{
   int i = 0;
   char c;
-  do {
+  do
+  {
     c = USART_get_char();
     out[++i] = c;
   } while (c);
 }
 
-static void USART_send_str(const char * str){
-  while (*str != 0){
+static void USART_send_str(const char *str)
+{
+  while (*str != 0)
+  {
     USART_send_char(*str);
     str++;
   }
 }
 
-
 ////////    Ble functions      ////////////
 
-
-void ble_get_data(char * str){
+void ble_get_data(char *str)
+{
   USART_get_data(str);
 }
 
-void bluetooth_init(){
-  USART_init();
+void bluetooth_init()
+{
+  USART_init( MYUBRR );
 }
 
-void ble_send_char(const char c){
-  USART_send_char( c );
+void ble_send_char(unsigned char c)
+{
+  USART_send_char(c);
 }
 
-void ble_send_str(const char * str){
-  USART_send_str( str );
+void ble_send_str(unsigned char *str)
+{
+  USART_send_str(str);
 }
 
 /////////////////////////////////////////////////
--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #define F_CPU 16000000
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -21,27 +20,24 @@
 
 void init_interrupt()
 {
-    //__enable_interrupt();
     sei();
-    //SREG |= (1<<_BV(SREG_I)); // Global interrupt enable
-    //MCUCR = (1<<_BV(IVCE));
 }
 
-// void SPI_MasterInit(void)
-// {
-//     /* Set MOSI and SCK output, all others input */
-//     DDRB = (1 << DDB3) | (1 << DDB5);
-//     /* Enable SPI, Master, set clock rate fck/16 */
-//     SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
-// }
+void SPI_MasterInit(void)
+{
+    /* Set MOSI and SCK output, all others input */
+    DDRB = (1 << DDB3) | (1 << DDB5);
+    /* Enable SPI, Master, set clock rate fck/16 */
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+}
 
-// void SPI_MasterTransmit(char cData)
-// {
-//     /* Start transmission */
-//     SPDR = cData;
-//     /* Wait for transmission complete */
-//     while (!(SPSR & (1 << SPIF)));
-// }
+void SPI_MasterTransmit(char cData)
+{
+    /* Start transmission */
+    SPDR = cData;
+    /* Wait for transmission complete */
+    while (!(SPSR & (1 << SPIF)));
+}
 
 
 
@@ -57,7 +53,7 @@ void main(){
   while (1){
     // get_time_str(t_str);
     
-    ble_send_char( 0x40 );
+    ble_send_str( "ouioui \n" );
     _delay_ms(1000);
   }
 
