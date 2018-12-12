e9ea2d3eef521378e3a90acf14d7781aca7cb927
ymenanteau <menanteau.yoann@hotmail.com>
2018-12-12 21:59:34 +0100

    ajout type Ffloat

---
+++ Ffloat.h
@@ -0,0 +1,26 @@
+#ifndef FFLOAT_H
+#define FFLOAT_H
+
+#define DIV_ACC 3
+
+typedef struct Ffloat {
+  int value;
+  int coma_index;
+} Ffloat;
+
+
+Ffloat ffloat_new(int value, int coma_index);
+Ffloat ffloat_add_ffloat(Ffloat a, Ffloat b);
+Ffloat ffloat_sub_ffloat(Ffloat a, Ffloat b);
+Ffloat int_add_ffloat(int a, Ffloat b);
+Ffloat int_sub_ffloat(int a, Ffloat b);
+Ffloat ffloat_add_int(Ffloat a, int b);
+Ffloat ffloat_sub_int(Ffloat a, int b);
+Ffloat ffloat_mult_ffloat(Ffloat a, Ffloat b);
+Ffloat int_mult_ffloat(int a, Ffloat b);
+Ffloat ffloat_mult_int(Ffloat a, int b);
+Ffloat ffloat_div_ffloat(Ffloat a, Ffloat b);
+Ffloat int_div_ffloat(int a, Ffloat b);
+Ffloat ffloat_div_int(Ffloat a, int b);
+
+#endif
